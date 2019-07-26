eacfca4f3e54299eb76a1da4385de65922ebe0be
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-26 11:06:20 -0500

    bounce box

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import aframe from 'aframe';
+
+aframe.registerComponent('log', {
+  schema: { type: 'string' },
+  init() {
+    console.log('logging', this.el.sceneEl.getAttribute('log'));
+  }
+});
