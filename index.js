6d3fa573180be17d84fdf3b50457e6f78d7232be
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-26 12:37:19 -0500

    async render box

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import aframe from 'aframe';
 
-aframe.registerComponent('log', {
+aframe.registerComponent('load-tree', {
   schema: { type: 'string' },
   init() {
-    console.log('logging', this.el.sceneEl.getAttribute('log'));
+    const newBox = document.createElement('a-box');
+    newBox.setAttribute('position', { x: -1, y: 1, z: -3 });
+    newBox.setAttribute('color', '#4CC3D9');
+    newBox.setAttribute('rotation', { x: 0, y: 10, z: 0 });
+    newBox.setAttribute('height', 2);
+    newBox.setAttribute('width', 3);
+
+    const sceneEl = document.querySelector('a-scene');
+    sceneEl.appendChild(newBox);
   }
 });
