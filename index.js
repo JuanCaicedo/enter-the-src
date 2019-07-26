a9335fff72bf515a409d5110f5bed6259e795cb2
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-26 14:07:38 -0500

    display files

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
-import aframe from 'aframe';
+import aframe, { utils, primitives } from 'aframe';
+import treeFixture from './tree-fixture';
+
+const meshMixin = primitives.getMeshMixin();
+
+aframe.registerPrimitive(
+  'a-file',
+  utils.extendDeep({}, meshMixin, {
+    defaultComponents: {
+      geometry: { primitive: 'box' }
+    },
+    mappings: {
+      depth: 'geometry.depth',
+      height: 'geometry.height',
+      width: 'geometry.width'
+    }
+  })
+);
+
+const displayBox = ({ sceneEl }) => {
+  const newBox = document.createElement('a-box');
+  newBox.setAttribute('position', { x: -1, y: 1, z: -3 });
+  newBox.setAttribute('color', '#4CC3D9');
+  newBox.setAttribute('rotation', { x: 0, y: 10, z: 0 });
+  newBox.setAttribute('height', 2);
+  newBox.setAttribute('width', 3);
+  newBox.setAttribute('material', 'side: double;');
+
+  sceneEl.appendChild(newBox);
+};
+
+const displayFile = ({ file, sceneEl, offset }) => {
+  const fileEl = document.createElement('a-file');
+  sceneEl.appendChild(fileEl);
+  fileEl.setAttribute('position', { x: offset, y: 1, z: -1 });
+  fileEl.setAttribute('color', 'red');
+  fileEl.setAttribute('rotation', { x: 0, y: 10, z: 0 });
+  fileEl.setAttribute('height', 0.2);
+  fileEl.setAttribute('width', 0.2);
+  fileEl.setAttribute('depth', 0.2);
+};
 
 aframe.registerComponent('load-tree', {
   schema: { type: 'string' },
   init() {
-    const newBox = document.createElement('a-box');
-    newBox.setAttribute('position', { x: -1, y: 1, z: -3 });
-    newBox.setAttribute('color', '#4CC3D9');
-    newBox.setAttribute('rotation', { x: 0, y: 10, z: 0 });
-    newBox.setAttribute('height', 2);
-    newBox.setAttribute('width', 3);
-
     const sceneEl = document.querySelector('a-scene');
-    sceneEl.appendChild(newBox);
+    displayBox({ sceneEl });
+
+    const files = treeFixture.tree.children.filter(
+      ({ type }) => type === 'file'
+    );
+    files.forEach((file, offset) => displayFile({ file, sceneEl, offset }));
   }
 });
---
+++ tree-fixture.js
@@ -0,0 +1,521 @@
+export default {
+  tree: {
+    path: '/Users/juan/code/personal/personal-blog',
+    name: 'personal-blog',
+    children: [
+      {
+        path: '/Users/juan/code/personal/personal-blog/.DS_Store',
+        name: '.DS_Store',
+        size: 10244,
+        extension: '',
+        type: 'file'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/.circleci',
+        name: '.circleci',
+        children: [
+          {
+            path:
+              '/Users/juan/code/personal/personal-blog/.circleci/config.yml',
+            name: 'config.yml',
+            size: 962,
+            extension: '.yml',
+            type: 'file'
+          }
+        ],
+        size: 962,
+        type: 'directory'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/.dir-locals.el',
+        name: '.dir-locals.el',
+        size: 152,
+        extension: '.el',
+        type: 'file'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/.nowignore',
+        name: '.nowignore',
+        size: 29,
+        extension: '',
+        type: 'file'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/README.md',
+        name: 'README.md',
+        size: 17,
+        extension: '.md',
+        type: 'file'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/api',
+        name: 'api',
+        children: [
+          {
+            path: '/Users/juan/code/personal/personal-blog/api/.DS_Store',
+            name: '.DS_Store',
+            size: 6148,
+            extension: '',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/api/index.js',
+            name: 'index.js',
+            size: 1393,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path:
+              '/Users/juan/code/personal/personal-blog/api/package-lock.json',
+            name: 'package-lock.json',
+            size: 88466,
+            extension: '.json',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/api/package.json',
+            name: 'package.json',
+            size: 437,
+            extension: '.json',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/api/posts',
+            name: 'posts',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/.DS_Store',
+                name: '.DS_Store',
+                size: 6148,
+                extension: '',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/console-log-spacemacs.mdx',
+                name: 'console-log-spacemacs.mdx',
+                size: 3007,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/copy-last-command.mdx',
+                name: 'copy-last-command.mdx',
+                size: 2000,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/css-unit-testing.mdx',
+                name: 'css-unit-testing.mdx',
+                size: 5609,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/elm-to-js.mdx',
+                name: 'elm-to-js.mdx',
+                size: 17839,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/first-talk-in-spanish.mdx',
+                name: 'first-talk-in-spanish.mdx',
+                size: 3714,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/mi-primera-charla-en-espanol.mdx',
+                name: 'mi-primera-charla-en-espanol.mdx',
+                size: 4008,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/modules-in-node.mdx',
+                name: 'modules-in-node.mdx',
+                size: 5331,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/starting-speaking.mdx',
+                name: 'starting-speaking.mdx',
+                size: 3388,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/tailored-css.mdx',
+                name: 'tailored-css.mdx',
+                size: 6328,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/tools.mdx',
+                name: 'tools.mdx',
+                size: 7157,
+                extension: '.mdx',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/api/posts/why-i-love-js-conf-co.mdx',
+                name: 'why-i-love-js-conf-co.mdx',
+                size: 4258,
+                extension: '.mdx',
+                type: 'file'
+              }
+            ],
+            size: 68787,
+            type: 'directory'
+          }
+        ],
+        size: 165231,
+        type: 'directory'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/notes',
+        name: 'notes',
+        children: [
+          {
+            path:
+              '/Users/juan/code/personal/personal-blog/notes/starting-speaking.org',
+            name: 'starting-speaking.org',
+            size: 3265,
+            extension: '.org',
+            type: 'file'
+          }
+        ],
+        size: 3265,
+        type: 'directory'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/now.json',
+        name: 'now.json',
+        size: 618,
+        extension: '.json',
+        type: 'file'
+      },
+      {
+        path: '/Users/juan/code/personal/personal-blog/www',
+        name: 'www',
+        children: [
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/.DS_Store',
+            name: '.DS_Store',
+            size: 6148,
+            extension: '',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/.babelrc.js',
+            name: '.babelrc.js',
+            size: 127,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/components',
+            name: 'components',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/components/Card.js',
+                name: 'Card.js',
+                size: 420,
+                extension: '.js',
+                type: 'file'
+              }
+            ],
+            size: 420,
+            type: 'directory'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/env-config.js',
+            name: 'env-config.js',
+            size: 239,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/layout',
+            name: 'layout',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/layout/Footer.js',
+                name: 'Footer.js',
+                size: 26,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/layout/Main.js',
+                name: 'Main.js',
+                size: 556,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/layout/Nav.js',
+                name: 'Nav.js',
+                size: 712,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/layout/blog.css',
+                name: 'blog.css',
+                size: 780,
+                extension: '.css',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/layout/global.css',
+                name: 'global.css',
+                size: 224,
+                extension: '.css',
+                type: 'file'
+              }
+            ],
+            size: 2298,
+            type: 'directory'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/lib',
+            name: 'lib',
+            children: [
+              {
+                path: '/Users/juan/code/personal/personal-blog/www/lib/gtag.js',
+                name: 'gtag.js',
+                size: 483,
+                extension: '.js',
+                type: 'file'
+              }
+            ],
+            size: 483,
+            type: 'directory'
+          },
+          {
+            path:
+              '/Users/juan/code/personal/personal-blog/www/package-lock.json',
+            name: 'package-lock.json',
+            size: 612356,
+            extension: '.json',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/package.json',
+            name: 'package.json',
+            size: 1543,
+            extension: '.json',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/pages',
+            name: 'pages',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/.DS_Store',
+                name: '.DS_Store',
+                size: 6148,
+                extension: '',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/_document.js',
+                name: '_document.js',
+                size: 1086,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/about.js',
+                name: 'about.js',
+                size: 2551,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path: '/Users/juan/code/personal/personal-blog/www/pages/blog',
+                name: 'blog',
+                children: [
+                  {
+                    path:
+                      '/Users/juan/code/personal/personal-blog/www/pages/blog/.DS_Store',
+                    name: '.DS_Store',
+                    size: 6148,
+                    extension: '',
+                    type: 'file'
+                  },
+                  {
+                    path:
+                      '/Users/juan/code/personal/personal-blog/www/pages/blog/index.js',
+                    name: 'index.js',
+                    size: 1015,
+                    extension: '.js',
+                    type: 'file'
+                  },
+                  {
+                    path:
+                      '/Users/juan/code/personal/personal-blog/www/pages/blog/post.js',
+                    name: 'post.js',
+                    size: 926,
+                    extension: '.js',
+                    type: 'file'
+                  }
+                ],
+                size: 8089,
+                type: 'directory'
+              },
+              {
+                path: '/Users/juan/code/personal/personal-blog/www/pages/es',
+                name: 'es',
+                children: [],
+                size: 0,
+                type: 'directory'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/guatemala.js',
+                name: 'guatemala.js',
+                size: 711,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/index.js',
+                name: 'index.js',
+                size: 1522,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/now.js',
+                name: 'now.js',
+                size: 1533,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/portland.js',
+                name: 'portland.js',
+                size: 1732,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/pages/resume.js',
+                name: 'resume.js',
+                size: 5729,
+                extension: '.js',
+                type: 'file'
+              }
+            ],
+            size: 29101,
+            type: 'directory'
+          },
+          {
+            path:
+              '/Users/juan/code/personal/personal-blog/www/postcss.config.js',
+            name: 'postcss.config.js',
+            size: 180,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/server.js',
+            name: 'server.js',
+            size: 923,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/static',
+            name: 'static',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/static/juan.jpg',
+                name: 'juan.jpg',
+                size: 26822,
+                extension: '.jpg',
+                type: 'file'
+              }
+            ],
+            size: 26822,
+            type: 'directory'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/tailwind.js',
+            name: 'tailwind.js',
+            size: 26370,
+            extension: '.js',
+            type: 'file'
+          },
+          {
+            path: '/Users/juan/code/personal/personal-blog/www/util',
+            name: 'util',
+            children: [
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/util/data.js',
+                name: 'data.js',
+                size: 513,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path:
+                  '/Users/juan/code/personal/personal-blog/www/util/data.test.js',
+                name: 'data.test.js',
+                size: 1216,
+                extension: '.js',
+                type: 'file'
+              },
+              {
+                path: '/Users/juan/code/personal/personal-blog/www/util/env.js',
+                name: 'env.js',
+                size: 128,
+                extension: '.js',
+                type: 'file'
+              }
+            ],
+            size: 1857,
+            type: 'directory'
+          }
+        ],
+        size: 708867,
+        type: 'directory'
+      }
+    ],
+    size: 889385,
+    type: 'directory'
+  }
+};
