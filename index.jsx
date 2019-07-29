8144f9f5461773d03cc8174a5d823fb5c59b0aee
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-29 12:03:41 -0500

    add directory name

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import * as React from 'jsx-dom';
 import aframe from 'aframe';
 import 'aframe-layout-component';
+import 'aframe-text-geometry-component';
 import './primitives/a-file';
 import treeFixture from './tree-fixture';
 import createElement from './create-element';
@@ -14,7 +15,7 @@ const Layout = ({ children }) => {
     <a-entity
       layout={{ type: 'circle', radius: 1 }}
       rotation={{ x: 90, y: 0, z: 0 }}
-      position={{ x: 0, y: 1, z: -2 }}
+      position={{ x: 0, y: 1, z: 0 }}
     >
       {children}
     </a-entity>
@@ -33,11 +34,33 @@ const Cylinder = ({ children }) => (
   </a-cylinder>
 );
 
+const Name = ({ name }) => (
+  <a-entity
+    center-x
+    text-geometry={{ value: `${name}/` }}
+    material={{ color: 'blue' }}
+    position={{ x: 0, y: 1, z: 0 }}
+  />
+);
+
 const Directory = ({ files, name }) => (
   <Cylinder>
+    <Name name={name} />
     <Layout>{files.map(file => <File file={file} />)}</Layout>
   </Cylinder>
 );
+
+aframe.registerComponent('center-x', {
+  update() {
+    // Set timeout because we need to wait for this to be loaded
+    setTimeout(() => {
+      let mesh = this.el.getObject3D('mesh');
+      let bbox = new THREE.Box3().setFromObject(this.el.object3D);
+      let offset = (bbox.min.x - bbox.max.x) / 2;
+      mesh.position.set(offset, 0, 0);
+    }, 0);
+  }
+});
 
 aframe.registerComponent('load-tree', {
   schema: { type: 'string' },
