7d420ee6cd1e8d1d335b8c1c2b47c4f3f5716632
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-29 12:33:03 -0500

    remove unnecessary returns

--- index.jsx
+++ index.jsx
@@ -6,21 +6,19 @@ import './primitives/a-file';
 import treeFixture from './tree-fixture';
 import createElement from './create-element';
 
-const File = ({ file, offset }) => {
-  return <a-file color="red" radius={0.2} rotation={{ x: 0, y: 0, z: 0 }} />;
-};
+const File = ({ file, offset }) => (
+  <a-file color="red" radius={0.2} rotation={{ x: 0, y: 0, z: 0 }} />
+);
 
-const Layout = ({ children }) => {
-  return (
-    <a-entity
-      layout={{ type: 'circle', radius: 1 }}
-      rotation={{ x: 90, y: 0, z: 0 }}
-      position={{ x: 0, y: 1, z: 0 }}
-    >
-      {children}
-    </a-entity>
-  );
-};
+const Layout = ({ children }) => (
+  <a-entity
+    layout={{ type: 'circle', radius: 1 }}
+    rotation={{ x: 90, y: 0, z: 0 }}
+    position={{ x: 0, y: 1, z: 0 }}
+  >
+    {children}
+  </a-entity>
+);
 
 const Cylinder = ({ children }) => (
   <a-cylinder
