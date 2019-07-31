90ad6f18e950f9c29c3c509664314513f2311cdb
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-07-31 15:14:50 -0500

    remove unused components

--- index.jsx
+++ index.jsx
@@ -41,7 +41,6 @@ const Cylinder = ({ children }) => (
     radius={10}
     material={{ side: 'double' }}
     position={{ x: 0, y: -1, z: -5 }}
-    grow-radius-for-children
     src="https://i.imgur.com/mYmmbrp.jpg"
   >
     {children}
@@ -72,56 +71,6 @@ aframe.registerComponent('center-x', {
       const bbox = new THREE.Box3().setFromObject(this.el.object3D);
       const offset = (bbox.min.x - bbox.max.x) / 2;
       mesh.position.set(offset, 0, 0);
-    }, 0);
-  }
-});
-
-aframe.registerComponent('center-y', {
-  update() {
-    // Set timeout because we need to wait for this to be loaded
-    setTimeout(() => {
-      const mesh = this.el.getObject3D('mesh');
-      const bbox = new THREE.Box3().setFromObject(this.el.object3D);
-      const offset = (bbox.max.y - bbox.min.y) / 2;
-      // Subtracting a little so that the cylinder will be lower than the floor.
-      // This is probably not good in that it messes up other math.
-      mesh.position.set(0, offset - 0.22, 0);
-    }, 0);
-  }
-});
-
-function distanceBetweenPoints(v1, v2) {
-  const dx = v1.x - v2.x;
-  const dz = v1.z - v2.z;
-
-  return Math.sqrt(dx * dx + dz * dz);
-}
-
-aframe.registerComponent('grow-radius-for-children', {
-  update() {
-    // Set timeout because we need to wait for this to be loaded
-    setTimeout(() => {
-      const mesh = this.el.getObject3D('mesh');
-      const bbox = new THREE.Box3().setFromObject(this.el.object3D);
-      const center = {
-        x: (bbox.min.x + bbox.max.x) / 2,
-        z: (bbox.min.z + bbox.max.z) / 2
-      };
-
-      const furthestDirection = R.pipe(
-        R.map(child => {
-          const mesh = child.getObject3D('mesh');
-          const bbox = new THREE.Box3().setFromObject(child.object3D);
-          return [
-            { x: bbox.min.x, z: bbox.min.z },
-            { x: bbox.max.x, z: bbox.max.z }
-          ];
-        }),
-        R.flatten,
-        R.map(point => distanceBetweenPoints(center, point)),
-        R.reduce(R.max, 0)
-      )(this.el.children);
-      this.el.setAttribute('radius', furthestDirection + 1);
     }, 0);
   }
 });
